27589dab98bb6dfdfb8b9bd6585877338d3ac0fd
lumleymason <lumleymason@gmail.com>
2025-03-10 11:43:48 -0500

    added react-select for searchable dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { TeamCard } from './teamCard';
 import { useState } from 'react';
 import { IPlayer, ITeamRelationship, gameStatus } from "./gameModel.mode";
 import { WinnerCard } from './winner';
+import Select, {SingleValue,ActionMeta} from 'react-select';
 
 
 
@@ -19,11 +20,14 @@ function App() {
 
 
 
-  const handlePlayerSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handlePlayerSelect = (newValue: SingleValue<{ label: string; value: string; }>, actionMeta: ActionMeta<{ label: string; value: string; }>) => {
 
-    let newPlayer = Logic.guessPlayer(e.target.value, player);
-    setStatus(Logic.status);
-    setPlayer({ ...newPlayer })
+    if(newValue){
+      let newPlayer = Logic.guessPlayer(newValue.value, player);
+      setStatus(Logic.status);
+      setPlayer({ ...newPlayer })
+    }
+
 
 
   }
@@ -56,18 +60,18 @@ function App() {
 
 
         <form className="max-w-sm mx-auto">
-          <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a player</label>
-          <select onChange={handlePlayerSelect} id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option disabled selected>Guess a player</option>
-
-            {Logic.players.map((p) => {
-              return (
-                <option >{p}</option>
-              )
-            })}
 
 
-          </select>
+      
+
+          <label htmlFor="playerDrop" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a player</label>
+          <Select   onChange={handlePlayerSelect} id="playerDrop" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        options= {Logic.players.map((p) => {
+          return (
+            {label: p, value: p}
+          )
+        })}
+      />
         </form>
 
 
